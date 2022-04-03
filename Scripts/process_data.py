65b64513a9d7280471f501cf88c84597b03b7d4f
Adrian Urbański <adrianu161999@gmail.com>
2022-04-03 16:17:08 +0200

    Redesign the process_data script
    It now accepts training and testing datasets as arguments, splitting will be
    done in a notebook instead.

--- Scripts/process_data.py
+++ Scripts/process_data.py
@@ -1,8 +1,7 @@
 import argparse
-import re
-from pathlib import Path
 
 import pandas as pd
+from pathlib import Path
 from sklearn.preprocessing import LabelEncoder
 from tqdm import tqdm
 
@@ -10,32 +9,32 @@ from tqdm import tqdm
 def parse_args():
     parser = argparse.ArgumentParser(description="Preprocess Amazon data.")
     parser.add_argument(
+        'train',
+        type=str,
+        help='pandas dataframe containing training interactions in parquet format'
+    )
+    parser.add_argument(
+        'test',
+        type=str,
+        help='pandas dataframe containing testing interactions in parquet format'
+    )
+    parser.add_argument(
         'path',
         type=str,
         help='path in which to store the results',
     )
-    parser.add_argument(
-        '--file',
-        type=str,
-        default=None,
-        help='only process the given parquet file; if empty processes every file',
-    )
-    parser.add_argument(
-        '--data_path',
-        dest='data_path',
-        type=str,
-        default='/pio/scratch/1/recommender_systems/interim/Amazon',
-        help='path in which data files are',
-    )
-
     return parser.parse_args()
 
 
-def remap_pdf(pdf, save_path):
+def remap_pdfs(train_pdf, test_pdf, save_path):
     user_encoder = LabelEncoder()
     item_encoder = LabelEncoder()
-    pdf['reviewerID'] = user_encoder.fit_transform(pdf['reviewerID'])
-    pdf['asin'] = item_encoder.fit_transform(pdf['asin'])
+
+    train_pdf['reviewerID'] = user_encoder.fit_transform(train_pdf['reviewerID'])
+    train_pdf['asin'] = item_encoder.fit_transform(train_pdf['asin'])
+
+    test_pdf['reviewerID'] = user_encoder.transform(test_pdf['reviewerID'])
+    test_pdf['asin'] = item_encoder.transform(test_pdf['asin'])
 
     user_list_pdf = pd.DataFrame(zip(user_encoder.classes_, range(len(user_encoder.classes_))))
     user_list_pdf.columns = ['org_id', 'remap_id']
@@ -45,31 +44,21 @@ def remap_pdf(pdf, save_path):
     item_list_pdf.columns = ['org_id', 'remap_id']
     item_list_pdf.to_csv(save_path / 'item_list.txt', sep=' ', index=False, header=True)
 
-    return pdf
+    return train_pdf, test_pdf
 
 
 def main():
     args = parse_args()
     tqdm.pandas()
 
-    data_root = Path(args.data_path)
     save_path = Path(args.path)
+    train_pdf = pd.read_parquet(args.train)
+    test_pdf = pd.read_parquet(args.test)
 
-    if args.file is None:
-        pattern = re.compile("^(?!meta).*clean\.parquet$")
-        data = []
-        for file in data_root.iterdir():
-            if pattern.match(file.name):
-                data.append(pd.read_parquet(file))
-        data_pdf = pd.concat(data)
-    else:
-        data_pdf = pd.read_parquet(data_root / args.file)
-
-    data_pdf = remap_pdf(data_pdf, save_path)
-
-    train_pdf = data_pdf.sample(frac=0.9)
-    test_pdf = data_pdf[~data_pdf.index.isin(train_pdf.index)]
+    # Drop users and items not present in the training set
     test_pdf = test_pdf[test_pdf['reviewerID'].isin(train_pdf['reviewerID']) & test_pdf['asin'].isin(train_pdf['asin'])]
+
+    train_pdf, test_pdf = remap_pdfs(train_pdf, test_pdf, save_path)
 
     grouped_train_pdf = train_pdf.groupby('reviewerID').progress_apply(
         lambda pdf: f"{pdf.iloc[0]['reviewerID']} " + ' '.join(map(str, pdf['asin']))
